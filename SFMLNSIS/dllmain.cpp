fef4ad51da3f1ba36a874d35147a2f9c76337aed
Ririto Ninigaya(Ryo Tabuki's Creator) <riritoxxl23@outlook.com>
2024-11-23 17:03:34 +0200

    Update dllmain.cpp

--- SFMLNSIS/dllmain.cpp
+++ SFMLNSIS/dllmain.cpp
@@ -2,12 +2,24 @@
 #include "includes.hpp"
 #include <SFML/Audio.hpp>
 #include <pluginapi.h>
-
-void __declspec(dllexport) PlayMusicWithLoop(LPCSTR mus_filename) {
+#define STATUS_SUCCESS 1
+#define EXITFAILED 5300
+#pragma comment(lib, "legacy_stdio_definitions.lib")
+bool isinitalizated = true;
+HINSTANCE g_hInstance;
+HWND g_hwndParent;
+extern "C" void __declspec(dllexport) PlayMusicWithLoop(HWND hwndParent, int string_size, LPTSTR variables, stack_t** stacktop, const char* mus_filename)
+{
+    EXDLL_INIT();
     sf::Music mus;
     if (mus.openFromFile(mus_filename)) {
         mus.setLoop(true);
         mus.play();
+    }
+    else 
+    {
+        MessageBoxA(0, "Failed!!!", "SFMLNSIS", MB_OK | MB_ICONERROR);
+        ExitProcess(1223);
     }
 }
 BOOL APIENTRY DllMain( HMODULE hModule,
@@ -18,7 +30,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(0, "This is My First Library for SFML(NSIS Programming Language for Windows Installer)... \nSo Enjoy to use this", "SFMLNSIS", MB_OK | MB_ICONINFORMATION);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
